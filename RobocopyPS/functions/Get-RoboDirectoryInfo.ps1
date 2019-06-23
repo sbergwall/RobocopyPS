66154498bab9e231761458b6e8c03390b9d386eb
unknown <simonbergwall@gmail.com>
2019-06-23 18:55:51 +0200

    added Get-RoboDirectoryInfo

---
+++ RobocopyPS/functions/Get-RoboDirectoryInfo.ps1
@@ -0,0 +1,52 @@
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+.NOTES
+   General notes
+#>
+
+function Get-RoboDirectoryInfo {
+
+    [CmdletBinding(SupportsShouldProcess = $true,
+        ConfirmImpact = 'Low')]
+
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true,
+            Position = 0)]
+        [ValidateNotNull()]
+        [ValidateNotNullOrEmpty()]
+        [String]$Source,
+
+        [switch]
+        $BackupMode
+    )
+
+    Begin {}
+
+    Process {
+        if ($pscmdlet.ShouldProcess('$Source', 'Get info')) {
+            try {
+                $PSBoundParameters.Add("Destination", "NULL")
+                $PSBoundParameters.Add("IncludeEmptySubDirectories", $true)
+                $PSBoundParameters.Add('List', $true)
+
+                Start-Robocopy @PSBoundParameters
+            }
+            catch {
+                $PSCmdlet.ThrowTerminatingError($PSitem)
+            }
+        }
+    }
+
+    End { }
+}
